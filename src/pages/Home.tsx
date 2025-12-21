523e964a691a0097ab641c071d2843b2ae728646
PasangDimdung <pasandimdung045@gmail.com>
2025-12-21 12:03:48 +0545

    added home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <section>
+      <h2>Personal Finance & Weather Dashboard (React SPA)</h2>
+
+      <p>
+        Track your daily expenses and check current weather information using
+        this single-page React application.
+      </p>
+
+    </section>
+  );
+};
+
+export default Home;
