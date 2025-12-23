1412e601688bf35e065a78312d83369b5b98fb59
PasangDimdung <pasandimdung045@gmail.com>
2025-12-23 14:11:58 +0545

    added local storage to store expenses and filter using query params

--- src/context/ExpenseContext.tsx
+++ src/context/ExpenseContext.tsx
@@ -1,5 +1,6 @@
-import { createContext, useContext, useState, useMemo, type ReactNode } from "react";
+import { createContext, useContext, useState, useMemo, type ReactNode, useEffect } from "react";
 import type { Expense } from "../models/Expense.type";
+import { useSearchParams } from "react-router-dom";
 
 interface ExpenseContextType {
   expenses: Expense[];
@@ -19,11 +20,45 @@ export const useExpenses = () => {
 };
 
 export const ExpenseProvider = ({ children }: { children: ReactNode }) => {
-  const [expenses, setExpenses] = useState<Expense[]>([]);
+  
+  const [expenses, setExpenses] = useState<Expense[]>(() => {
+    const saved = localStorage.getItem("expenses");
+    return saved ? JSON.parse(saved) : [];
+  });
+
   const [categoryFilter, setCategoryFilter] = useState("");
   const [dateFilter, setDateFilter] = useState("");
 
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const category = searchParams.get("category") || "";
+    const date = searchParams.get("date") || "";
+    setCategoryFilter(category);
+    setDateFilter(date);
+  }, []);
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+
+    if (categoryFilter) params.set("category", categoryFilter);
+    else params.delete("category");
+
+    if (dateFilter) params.set("date", dateFilter);
+    else params.delete("date");
+
+    // Only update URL if changed
+    if (params.toString() !== searchParams.toString()) {
+      setSearchParams(params, { replace: true }); 
+    }
+  }, [categoryFilter, dateFilter, searchParams, setSearchParams]);
+
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
   const addExpense = (expense: Expense) => setExpenses((prev) => [...prev, expense]);
+
   const resetFilters = () => {
     setCategoryFilter("");
     setDateFilter("");
